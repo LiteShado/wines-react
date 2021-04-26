858203f9dbf408d5d19c294ccb4a12c15e869a14
Natalie <theliteshado@icloud.com>
2021-04-26 10:00:26 -0400

    edits

--- nat-wines/src/App.js
+++ nat-wines/src/App.js
@@ -45,50 +45,17 @@ function App() {
       />
 
       <Route path = '/wines/:id' render={(props) => {
-        const wine = AllWines.find(wine => wine.id.toString() === props.match.params.id)
-
-        props = {...props, ...wine}
-
-        return
-
-        <Wines {...props } url = {apiUrl}
         />
 
       }}/>
 
       <Route path = '/create' render={() => <CreateWine url = {apiUrl}
       getWines={getWines}
-      Name = {Name}
-      Year = {Year}
-      Country = {Country}
-      Picture = {Picture}
-      setName = {setName}
-      setYear = {setYear}
-      setCountry = {setCountry}
-      setPicture = {setPicture} />}
       />
 
-      <Route path = '/edit/:id' render={(props) => {
-        const wine = AllWines.find(wine => wine.id.toString() === props.match.params.id)
-        props = {...props, ...wine}
-
-        return
-        <EditWine {...props }
-        Name = {Name}
-        Year = {Year}
-        Country = {Country}
-        Picture = {Picture}
-        setName = {setName}
-        setYear = {setYear}
-        setCountry = {setCountry}
-        setPicture = {setPicture}
-
-        url = {apiUrl} getWines={getWines}/>
-      }}/>
 
 
     </div>
-  );
 }
 
 export default App;
